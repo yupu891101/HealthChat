d8505e4bbaa92779ca16dd96b839be011bb66e12
Hsu Yu Pu <yupu891101@gmail.com>
2024-08-21 15:59:24 +0000

    Updated file

---
+++ install.bash
@@ -0,0 +1,11 @@
+pip install git+https://github.com/suno-ai/bark.git
+pip install git+https://github.com/huggingface/transformers.git
+pip install OpenCC
+pip install soundfile
+pip install langid
+pip install nltk
+pip install gradio==4.33.0
+pip install git+https://github.com/openai/whisper.git 
+pip install ruamel.yaml
+pip install 'accelerate>=0.26.0'
+pip install -U bitsandbytes
